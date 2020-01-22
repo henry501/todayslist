b234f56413465273bb7052b7acf15c99ab54395d
John Patterson <john.h.patterson1@gmail.com>
2020-01-22 12:27:44 +0100

    added template dir to settings, verified first run

--- todayslist/todayslist/settings.py
+++ todayslist/todayslist/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'todayslist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates"), os.path.join(BASE_DIR, "todayslist/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
